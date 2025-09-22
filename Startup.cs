8c959c8b18317db4e2e3e0ddd6e652c8febed4c7
MgKyaw <mgkyaw20112@gmail.com>
2025-09-22 21:03:18 +0700

    feat: Configure db context in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ProductMicroservice.DBContexts;
 
 namespace ProductMicroservice
 {
@@ -26,6 +28,7 @@ namespace ProductMicroservice
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<ProductContext>(o => o.UseSqlServer(Configuration.GetConnectionString("ProductDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
