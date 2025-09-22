a5c4eae11187696a95e5622c2d5d220932c66c4f
MgKyaw <mgkyaw20112@gmail.com>
2025-09-22 21:08:02 +0700

    feat: Create IProductRepository

---
+++ Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductMicroservice.Models;
+using System.Collections.Generic;
+
+namespace ProductMicroservice.Repository
+{
+    interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductById(int productId);
+        void InsertProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int productId);
+        void Save();
+    }
+}
